f84e8138bd9ce3f1efc364f93b979e35382f0aaa
bjames967 <46580502+bjames967@users.noreply.github.com>
2019-06-19 14:08:25 -0400

    Update TVshow.ts
    added detail section to TV show

--- src/app/models/TVshow.ts
+++ src/app/models/TVshow.ts
@@ -10,7 +10,43 @@ export interface TVshow {
     backdrop_path: string;
    
     //detail
-    }
+    created_by?: {
+        id?: number;
+        credit_id?: number;
+        name?: string;
+        gender?: number;
+        profile_path?: string;
+    }[];
+    homepage?: string;
+    last_episode_to_air?: {
+        air_date?: string;
+        episode_number?: 10;
+        name?: string;
+        season_number?: number;
+        show_id?: number;
+        still_path?: string;
+    }[];
+    networks: {
+        name?: string;
+        logog_path?: strinng';
+    }[];
+    number_of_epsiodes?: number;
+    number_of_seasons?: number;
+    production_compaines?: {
+        id?: number;
+        logo_path?: string;
+        name?: string;
+    }[];
+    seasons?: {
+        air_date?: string;
+        epsiode_count?: number;
+        name?: string;
+        overview?: string;
+        poster_path?: string;
+        season_number?: number;
+    }[];
+    
+}
     
     
     
