9b4d19e39a7eb2131f9b17ae8403887f8a3587ca
bjames967 <46580502+bjames967@users.noreply.github.com>
2019-06-21 10:30:32 -0400

    Update TVshow.ts
    seperating TVshow and TVshow details

--- src/app/models/TVshow.ts
+++ src/app/models/TVshow.ts
@@ -1,51 +1,17 @@
 export interface TVshow {
+    original_name: string;
     id: number;
     name: string;
-    original_name: string;
-    overview: string;
-    poster_path: string;
     vote_count: number;
     vote_average: number;
-    first_air_date: string
+    poster_path: string;
+    first_air_date: string;
+    popularity: number;
+    genre_ids: number[];
+    original_language: string;
     backdrop_path: string;
-   
-    //detail
-    created_by?: {
-        id?: number;
-        credit_id?: number;
-        name?: string;
-        gender?: number;
-        profile_path?: string;
-    }[];
-    homepage?: string;
-    last_episode_to_air?: {
-        air_date?: string;
-        episode_number?: 10;
-        name?: string;
-        season_number?: number;
-        show_id?: number;
-        still_path?: string;
-    }[];
-    networks: {
-        name?: string;
-        logos_path?: string;
-    }[];
-    number_of_epsiodes?: number;
-    number_of_seasons?: number;
-    production_compaines?: {
-        id?: number;
-        logo_path?: string;
-        name?: string;
-    }[];
-    seasons?: {
-        air_date?: string;
-        epsiode_count?: number;
-        name?: string;
-        overview?: string;
-        poster_path?: string;
-        season_number?: number;
-    }[];
-    
+    overview: string;
+    origin_country: string[];   
 }
     
     
