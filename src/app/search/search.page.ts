0776d3f9ba45fe456a1efee50a46f95d8cbc0142
bjames967 <46580502+bjames967@users.noreply.github.com>
2019-07-02 10:54:43 -0400

    add loading feature for search

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -4,6 +4,7 @@ import { Movie } from './../models/Movie'
 import { TVshow } from './../models/TVshow'
 import { Actor } from './../models/Actor'
 import { Router } from '@angular/router'
+import { LoadingController, Content } from '@ionic/angular';
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
@@ -18,7 +19,9 @@ export class SearchPage implements OnInit {
   searchInput = '';
   results: Movie[] | TVshow[] | Actor[];
   
-  constructor(private movieservice: MoviesService, private router: Router ) { }
+  constructor(private movieservice: MoviesService, 
+              private router: Router
+              private loadController: LoadingController) { }
 
   ngOnInit() {
     this.searchType = 'movies';
@@ -50,18 +53,26 @@ export class SearchPage implements OnInit {
   }
 
   private performSearch(query: string) {
+    let loader;
     if (!query || query.trim().length <= 0) { return; }
-        switch (this.searchType) {
-
-        case 'movies':
-            this.performSearchMovies(query); break;   
-        case 'tv':
-          this.performSearchTv(query); break;
-        case 'person':
-          this.performSearchActor(query); break;
-        default:
-        }
-    }
+      switch (this.searchType) {
+        case 'movies': this.performSearchMovies(query); break;   
+        case 'tv': this.performSearchTv(query); break;     
+      }
+    const loadingView = { translucent: true, spinner: 'dots'};
+    const load = await this.loadController.create(loadingView);
+    load.present();
+    loader.subscribe(res => {
+      if(!this.results){
+         this.results = [];
+      }
+      this.results = this.results.concat(res);
+      load.dismiss();
+    }, err => {
+      this.results = [];
+      load.dismiss();
+    });
+  }
 
     private performSearchMovies(query: string) {
       this.movieservice.searchMovies(query).subscribe(res => {
@@ -74,15 +85,7 @@ export class SearchPage implements OnInit {
       this.movieservice.searchTv(query).subscribe(res => {
         this.results = res;
         console.log(this.results)
-      })
-    }
-
-    private performSearchActor(query: string){
-      this.movieservice.searchPerson(query).subscribe(res => {
-        this.results = res;
-        console.log(this.results)
       });
     }
-
 }
 
