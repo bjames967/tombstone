fc72d41b4f639d7ab85c2c9c5412861f28c3a3bb
bjames967 <46580502+bjames967@users.noreply.github.com>
2019-07-09 09:09:01 -0400

    Update sorting.service.ts

--- src/app/service/sorting.service.ts
+++ src/app/service/sorting.service.ts
@@ -1,1 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Movie } from './../models/Movie';
+import { TVshow } from './../models/TVshow';
+import { StorageUnit } from './../models/StorageUnit'
+import { TVdetails } from '../models/TVdetails';
+import { StorageService } from '../storage.service';
 
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+
+  
+  constructor(private storageService: StorageService){}
+  
+  //sorted in aplhabetical order
+  sortByName(savedMovies: StorageUnit[]){
+    
+    
+  }
+  //This rating is from the API, not the rating they made
+  sortByRating(savedMovies: StorageUnit[]){
+    
+  }
+  //for tombstones only as watchlist isn't giving a rating by the user
+  sortByPersonalRating(savedMovies: StorageUnit[]){
+    
+  }
+  //either runtime of movie or episode length
+  sortByRunTime(savedMovies: StorageUnit[]){
+    
+  }
+  //sort by when movie/show was released by newest dates to oldest dates
+  sortByReleaseDate(savedMovies: StorageUnit[]){
+    
+  }
+  //sort by when the user added the unit to their tombstone library
+  sortBySavedDate(savedMovies: StorageUnit[]){
+    
+  } 
+}
