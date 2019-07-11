b83d787a636ee79024918271aec61735cb16e7f7
bjames967 <46580502+bjames967@users.noreply.github.com>
2019-07-11 13:52:34 -0400

    added video extension to TV

--- src/app/models/TVdetails.ts
+++ src/app/models/TVdetails.ts
@@ -66,4 +66,16 @@ export interface TVdetails {
      type: string;
      vote_average: number;
      vote_count: number;
+     videos?: {
+         results?: {
+             id?: string;
+             iso_639_1?: string;
+             iso_3166_1?: string;
+             key?: string;
+             name?: string;
+             site?: string;
+             size?: number;
+             tyoe?: string;
+         }[];
+     };
 }
