9cecf77800d7cb1e0e1db29dc3ad9400ebc71cc6
bjames967 <46580502+bjames967@users.noreply.github.com>
2019-07-15 15:02:20 -0400

    Update favorites.page.ts

--- src/app/favorites/favorites.page.ts
+++ src/app/favorites/favorites.page.ts
@@ -7,9 +7,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritesPage implements OnInit {
 
-  constructor() { }
+  favoriteType: 'movies' | 'tv';
+  results: StorageUnit[];
+  editMode: boolean;
+  
+  constructor(private movieService: MoviesService,
+              private storageService: StorageService,
+              private router: Router,
+              private popoverCtrl: PopoverController) { }
 
-  ngOnInit() {
+   ngOnInit() {
+    this.stoneType = 'movies';
+    this.onStoneChanged();
+
+  }
+  
+  enterEditMode(){
+    this.editMode = true;
+    this.onStoneChanged();
+  }
+  
+  exitEditMode(){
+    this.editMode = false;
+    this.onStoneChanged();
   }
 
-}
+  onStoneChanged(){
+    this.results = null;
+    this.pageSwap();
+  }
+
+  pageSwap(){
+    switch(this.stoneType){
+      case 'movies':
+        this.loadMovieFavorites(); break;
+      case 'tv':
+        this.loadTvFavorites(); break;
+      default:
+    }
+  }
+  
+  async openSortingModal(ev: any) {
+    const popover = await this.popoverCtrl.create({
+      component: SortPagePage,
+      event: ev,
+      translucent: true,
+      showBackdrop: true,
+      animated: true
+    });
+    return await popover.present();
+  }
+  
+  async openTombstoneOptionsModal(ev: any){
+  }
+
+  
+  onMovieClick(id){
+    this.router.navigate(['movie', id])
+  }
+  
+  onTvClick(id){
+    this.router.navigate(['tv', id]);
+  }
+  
+  
+  loadTvFavorites(){
+    this.storageService.getFavorites().then((list: StorageUnit[]) => {
+      this.results = list;
+    });
+  }
+  
+  loadMovieFavorites(){
+    this.storageService.getMovieFavorites().then((list: StorageUnit[]) => {
+      this.results = list;
+    });
+  }
+  
+  removeTvFavorite(id: number){
+    this.storageService.deleteMovieFromFavorites(id);
+    this.loadMovieFavorites();
+  }
+  
+  removeMovieFavorite(id: number){
+    this.storageService.deleteTvFromFavorites(id);
+    this.loadTvFavorites();
+  }
