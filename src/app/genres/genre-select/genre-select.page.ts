2a7d86cca29010ffa09673f1996631f4dd873359
bjames967 <46580502+bjames967@users.noreply.github.com>
2019-07-16 10:58:51 -0400

    Update genre-select.page.ts

--- src/app/genres/genre-select/genre-select.page.ts
+++ src/app/genres/genre-select/genre-select.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+
 
 @Component({
   selector: 'app-genre-select',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenreSelectPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+  
+  onShowGenreClick(id: number){
+    this.router.navigate([type, 'movie', id]);
+  }
+  
+  onMovieGenreClick(id: number){
+     this.router.navigate(['t', 'Show', id]);
+  }
+  
 
 }
